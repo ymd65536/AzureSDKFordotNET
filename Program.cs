18c805af11ff558e82de82d6b4708be1a4278d63
ymd65536 <ymd65536@gmail.com>
2024-05-24 17:02:54 +0900

    add: simple prompt

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using Azure.Identity;
 using Microsoft.SemanticKernel;
-using System.ComponentModel;
 
 var ModelName = Environment.GetEnvironmentVariable("MODEL_NAME")!;
 var Endpoint = Environment.GetEnvironmentVariable("OPEN_AI_ENDPOINT")!;
@@ -14,23 +13,8 @@ builder.AddAzureOpenAIChatCompletion(
 var kernel = builder.Build();
 
 var prompt = """
-    今日の日付を教えてください。
-
-    ## 参考情報
-    {{ CreateListItemPlugin.CreateItem categoryName='今日の日付' value='2023/12/25' }}
-    """;
-
+あなたは誰ですか？
+""";
+// 実行して結果を表示
 var result = await kernel.InvokePromptAsync(prompt);
 Console.WriteLine(result.GetValue<string>());
-
-class CreateListItemPlugin
-{
-    [KernelFunction]
-    [Description("リストの1項目を生成します。")]
-    public string CreateItem(
-        [Description("カテゴリ名")]
-        string categoryName,
-        [Description("カテゴリの値")]
-        string value) =>
-        $"- {categoryName}: {value}";
-}
